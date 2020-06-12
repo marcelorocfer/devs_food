6d86a368685cf1edaf48db5bd76c50dd8759aba6
Marcelo <marceloengecomp@gmail.com>
2020-06-12 16:45:17 -0300

    Home

---
+++ src/components/Header/index.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { Container, Logo, SearchInput } from './styled';
+
+export default ({ search, onSearch }) => {
+    const [inputActive, setinputActive] = useState(search == '' ? false : true);
+
+    const handleInputFocus = () => {
+        setinputActive(true);
+    }
+
+    const handleInputBlur = () => {
+        if (search == '') {
+            setinputActive(false);
+        }
+    }
+
+    const handleChange = (e) => {
+        onSearch( e.target.value );
+    }
+
+    return (
+        <Container>
+            <Logo src="/assets/logo.png" />
+            <SearchInput 
+                type="text" 
+                placeholder="Digite um produto..." 
+                value={search}
+                onChange={handleChange}
+                active={inputActive}
+                onFocus={handleInputFocus}
+                onBlur={handleInputBlur}
+            />
+        </Container>
+    );
+}
