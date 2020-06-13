76bdf107835f66a6bc2db17deeec1af4f932463a
Marcelo <marceloengecomp@gmail.com>
2020-06-13 19:28:09 -0300

    Paginação

--- src/pages/HomeScreen/index.js
+++ src/pages/HomeScreen/index.js
@@ -6,7 +6,9 @@ import {
     CategoryArea, 
     CategoryList,
     ProductArea,
-    ProductList
+    ProductList,
+    ProductPaginationArea,
+    ProductPaginationItem
 } from './styled';
 
 import api from '../../api';
@@ -20,13 +22,17 @@ export default () => {
     const [headerSearch, setheaderSearch] = useState('');
     const [categories, setCategories] = useState([]);
     const [products, setProducts] = useState([]);
+    const [totalPages, setTotalPages] = useState(0);
 
     const [activeCategory, setActiveCategory] = useState(0);
+    const [activePage, setActivePage] = useState(0);
 
     const getProducts = async () => {
         const prods = await api.getProducts();
         if (prods.error === '') {
             setProducts(prods.result.data);
+            setTotalPages(prods.result.pages);
+            setActivePage(prods.result.page);
         }
     }
 
@@ -42,8 +48,9 @@ export default () => {
     }, []);
 
     useEffect(()=>{
+        setProducts([]);
         getProducts();
-    }, [activeCategory]);
+    }, [activeCategory, activePage]);
 
     return (
         <Container>
@@ -85,6 +92,21 @@ export default () => {
                         ))}
                     </ProductList>
                 </ProductArea>            
+            }
+
+            {totalPages > 0 &&
+                 <ProductPaginationArea>
+                     {Array(8).fill(0).map((item, index)=>(
+                        <ProductPaginationItem 
+                            key={index} 
+                            active={activePage}
+                            current={index + 1}
+                            onClick={()=>setActivePage(index + 1)}
+                        >
+                            {index + 1}
+                         </ProductPaginationItem>
+                     ))}
+                 </ProductPaginationArea>
             }
         </Container>
     );
--- src/pages/HomeScreen/styled.js
+++ src/pages/HomeScreen/styled.js
@@ -25,3 +25,18 @@ export const ProductList = styled.div`
     grid-template-columns: repeat(3, 1fr);
     grid-gap: 15px;
 `;
+
+export const ProductPaginationArea = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 20px;
+`;
+
+export const ProductPaginationItem = styled.div`
+    background-color: ${props=>props.active === props.current ? '#FFF' : '#AAE09A'};
+    padding: 5px 10px;
+    border-radius: 5px;
+    box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.16);
+    cursor: pointer;
+    margin-right: 10px;
+`;
