90365009563be0de935e1a7e74c2546dc0075b16
Marcelo <marceloengecomp@gmail.com>
2020-06-13 11:59:29 -0300

    Exibição das categorias

--- src/pages/HomeScreen/index.js
+++ src/pages/HomeScreen/index.js
@@ -1,26 +1,66 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from "react-router-dom";
-import { Container } from './styled';
+import { 
+    Container, 
+    CategoryArea, 
+    CategoryList 
+} from './styled';
 
 import api from '../../api';
 
 import Header from '../../components/Header';
+import CategoryItem from '../../components/CategoryItem';
 
 export default () => {
     const history = useHistory();
     const [headerSearch, setheaderSearch] = useState('');
+    const [categories, setCategories] = useState([]);
+
+    const [activeCategory, setActiveCategory] = useState(0);
 
     useEffect(()=>{
         const getCategories = async () => {
-            const categories = await api.getCategories();
+            const cat = await api.getCategories();
+            if (cat.error === '') {
+                setCategories(cat.result);
+            }
         };
 
         getCategories();
     }, []);
 
+    useEffect(()=>{
+
+    }, [activeCategory]);
+
     return (
         <Container>
             <Header search={headerSearch} onSearch={setheaderSearch} />
+
+            {categories.length > 0 &&
+                <CategoryArea>
+                    Selecione uma categoria
+                    <CategoryList>
+                        <CategoryItem 
+                            data={{
+                                id:0, 
+                                title:'Todas as categorias', 
+                                image:'/assets/food-and-restaurant.png'
+                            }}
+                            activeCategory={activeCategory}
+                            setActiveCategory={setActiveCategory}
+                        />
+                        {categories.map((item, index)=>(
+                            <CategoryItem 
+                                key={index} 
+                                data={item} 
+                                activeCategory={activeCategory}
+                                setActiveCategory={setActiveCategory}
+                            />
+                        ))}
+                    </CategoryList>
+                </CategoryArea>
+            }
         </Container>
     );
 }
