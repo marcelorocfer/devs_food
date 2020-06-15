5624bd3b90595204c70cee917524ae1fb1275749
Marcelo <marceloengecomp@gmail.com>
2020-06-15 00:18:53 -0300

    Criação do componente modal

--- src/pages/HomeScreen/index.js
+++ src/pages/HomeScreen/index.js
@@ -16,6 +16,7 @@ import api from '../../api';
 import Header from '../../components/Header';
 import CategoryItem from '../../components/CategoryItem';
 import ProductItem from '../../components/ProductItem';
+import Modal from '../../components/Modal';
 
 let searchTimer = null;
 
@@ -25,6 +26,8 @@ export default () => {
     const [categories, setCategories] = useState([]);
     const [products, setProducts] = useState([]);
     const [totalPages, setTotalPages] = useState(0);
+    
+    const [modalStatus, setModalStatus] = useState(false);
 
     const [activeCategory, setActiveCategory] = useState(0);
     const [activePage, setActivePage] = useState(1);
@@ -118,6 +121,14 @@ export default () => {
                      ))}
                  </ProductPaginationArea>
             }
+
+            <Modal status={modalStatus} setStatus={setModalStatus}>
+                Conteúdo do Modal
+                <div style={{backgroundColor: '#FF0000', width: 400, height: 400}}>
+
+                </div>
+                Fim do Conteúdo
+            </Modal>
         </Container>
     );
 }
